d7c674e18f3f0d1748468b470e2fa89a37385d79
balci796881 <mariamostofa@yahoo.com>
2022-01-28 16:39:54 -0800

    I styled it

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyCClR07pihbkcZfSIvD610qp35zQGmuunM",
+    authDomain: "voting-app-8ade9.firebaseapp.com",
+    databaseURL: "https://voting-app-8ade9-default-rtdb.firebaseio.com",
+    projectId: "voting-app-8ade9",
+    storageBucket: "voting-app-8ade9.appspot.com",
+    messagingSenderId: "295252946782",
+    appId: "1:295252946782:web:0eb3284363a08dea2369cf"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
